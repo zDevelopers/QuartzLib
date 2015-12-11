fc9fbeed6d3d73fd59738abb7b80307399c77639
Amaury Carrade <amaury@carrade.eu>
2015-12-11 16:21:53 +0100

    Added auto-save capabilities to the configuration API, and javadoc.
    * NEW: added a set(T value, Boolean save) method to the ConfigurationItems. The set(value) method becomes an alias for set(value, true).
    * NEW: added javadoc to the ConfigurationItem class.

--- src/main/java/fr/zcraft/zlib/components/configuration/ConfigurationItem.java
+++ src/main/java/fr/zcraft/zlib/components/configuration/ConfigurationItem.java
@@ -12,39 +12,85 @@ import org.bukkit.configuration.file.FileConfiguration;
 import java.util.List;
 
 
+/**
+ * Represents a configuration item stored in the config.yml file.
+ *
+ * @param <T> The type of the stored value.
+ */
 public class ConfigurationItem<T>
 {
     private final String fieldName;
     private final T defaultValue;
     private final String[] deprecatedNames;
-    
+
+    /**
+     * @param fieldName The path of the field in the {@code config.yml} file.
+     * @param defaultValue The default value if this is not defined.
+     * @param deprecatedNames A list of deprecated names to migrate the old values automatically.
+     */
     public ConfigurationItem(String fieldName, T defaultValue, String ... deprecatedNames)
     {
         this.fieldName = fieldName;
         this.defaultValue = defaultValue;
         this.deprecatedNames = deprecatedNames;
     }
-    
+
+    /**
+     * @return the defined value for this configuration item, or the default value if missing.
+     */
     public T get()
     {
         return get(fieldName, defaultValue);
     }
-    
+
+    /**
+     * @return the default value for this configuration item.
+     */
     public T getDefaultValue()
     {
         return defaultValue;
     }
-    
+
+    /**
+     * @return {@code true} if a value is explicitly set in the configuration file.
+     */
     public boolean isDefined()
     {
         return getConfig().contains(fieldName);
     }
 
+    /**
+     * Updates the value of this configuration item. Saves the change in the configuration file.
+     *
+     * If you don't want to save the update, use {@link #set(Object,Boolean) set(value, false)}.
+     *
+     * @param value the new value.
+     * @return The previously stored value.
+     *
+     * @see #set(Object, Boolean)
+     */
     public T set(T value)
     {
-        T oldValue = get();
+        return set(value, true);
+    }
 
+    /**
+     * Updates the value of this configuration item.
+     *
+     * @param value the new value.
+     * @param save {@code true} to save this change in the config file. Be aware that it will save all unsaved changes,
+     *             including previous values changed with this argument set to {@code false}.
+     *
+     * @return The previously stored value.
+     */
+    public T set(T value, Boolean save)
+    {
+        T oldValue = get();
         getConfig().set(fieldName, value);
+
+        if (save)
+            Configuration.save();
+
         return oldValue;
     }
     
@@ -105,7 +151,18 @@ public class ConfigurationItem<T>
         else
             return (T) getConfig().get(path, defaultValue);
     }
-    
+
+
+    /**
+     * Utility method to construct a configuration item.
+     *
+     * @param fieldName The path of the field in the {@code config.yml} file.
+     * @param defaultValue The default value if this is not defined.
+     * @param deprecatedNames A list of deprecated names to migrate the old values automatically.
+     * @param <T> The type of the stored value.
+     *
+     * @return A ready-to-use configuration item.
+     */
     static public <T> ConfigurationItem<T> item(String fieldName, T defaultValue, String... deprecatedNames)
     {
         return new ConfigurationItem<>(fieldName, defaultValue, deprecatedNames);
