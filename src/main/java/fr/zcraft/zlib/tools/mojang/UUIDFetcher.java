8763722028d72a8f6a48ed403afa9dc7c6de9219
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2016-05-07 23:56:52 +0200

    * BUG: UUIDFetcher: Correctly handle 204 (No content) responses.
    Returning the correct type is helpful, I guess.

--- src/main/java/fr/zcraft/zlib/tools/mojang/UUIDFetcher.java
+++ src/main/java/fr/zcraft/zlib/tools/mojang/UUIDFetcher.java
@@ -214,6 +214,8 @@ public final class UUIDFetcher
 			throw new IOException("Invalid response from server, unable to parse received JSON : " + ex.toString());
 		}
 
+                if(array == null) return uuidMap;
+                
 		for (Object profile : array)
 		{
 			JSONObject jsonProfile = (JSONObject) profile;
@@ -301,7 +303,9 @@ public final class UUIDFetcher
 		{
 			throw new IOException("Invalid response from server, unable to parse received JSON : " + ex.toString());
 		}
-
+                
+                if(object == null) return null;
+                
 		User user = new User();
 		user.name = (String) object.get("name");
 		user.uuid = fromMojangUUID((String) object.get("id"));
@@ -376,7 +380,7 @@ public final class UUIDFetcher
 	private static Object readResponse(HttpURLConnection connection) throws IOException, ParseException
 	{
             if(connection.getResponseCode() == 204)
-                return new JSONArray();
+                return null;
             
             return new JSONParser().parse(new InputStreamReader(connection.getInputStream()));
 	}
