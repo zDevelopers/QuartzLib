02a59142d7e83000498327266a6b3354246d2be2
Amaury Carrade <amaury@carrade.eu>
2020-11-26 14:08:45 +0100

    Fixed both failing tests
    - QuartzLibTest failed because it was executed in JUnit 5 context instead
      of JUnit 4.
    - ItemStackBuilderTest failed because we were on a too old MockBukkit version.

--- src/test/java/fr/zcraft/quartzlib/core/QuartzLibTest.java
+++ src/test/java/fr/zcraft/quartzlib/core/QuartzLibTest.java
@@ -3,7 +3,7 @@ package fr.zcraft.quartzlib.core;
 import static org.junit.Assert.assertSame;
 
 import fr.zcraft.quartzlib.MockedToasterTest;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 
 public class QuartzLibTest extends MockedToasterTest {
