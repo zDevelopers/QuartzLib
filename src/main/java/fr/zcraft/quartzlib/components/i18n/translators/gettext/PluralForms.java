869d062abe743339bff416ea1ce15286b6f0cc59
Amaury Carrade <amaury@carrade.eu>
2021-04-12 03:31:00 +0200

    Added a bunch of known plural rules
    Reducing the probability of the JS engine usage. If we never load this,
    it's better. All POEdit-generated rules were added.

--- src/main/java/fr/zcraft/quartzlib/components/i18n/translators/gettext/PluralForms.java
+++ src/main/java/fr/zcraft/quartzlib/components/i18n/translators/gettext/PluralForms.java
@@ -1,35 +1,37 @@
 /*
- * Plugin UHCReloaded : Alliances
+ * Copyright or © or Copr. Moribus (2013)
+ * Copyright or © or Copr. ProkopyL <prokopylmc@gmail.com> (2015)
+ * Copyright or © or Copr. Amaury Carrade <amaury@carrade.eu> (2016 – 2021)
+ * Copyright or © or Copr. Vlammar <valentin.jabre@gmail.com> (2019 – 2021)
  *
- * Copyright ou © ou Copr. Amaury Carrade (2016)
- * Idées et réflexions : Alexandre Prokopowicz, Amaury Carrade, "Vayan".
+ * This software is a computer program whose purpose is to allow insertion of
+ * custom images in a Minecraft world.
  *
- * Ce logiciel est régi par la licence CeCILL soumise au droit français et
- * respectant les principes de diffusion des logiciels libres. Vous pouvez
- * utiliser, modifier et/ou redistribuer ce programme sous les conditions
- * de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
- * sur le site "http://www.cecill.info".
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
  *
- * En contrepartie de l'accessibilité au code source et des droits de copie,
- * de modification et de redistribution accordés par cette licence, il n'est
- * offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
- * seule une responsabilité restreinte pèse sur l'auteur du programme,  le
- * titulaire des droits patrimoniaux et les concédants successifs.
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
  *
- * A cet égard  l'attention de l'utilisateur est attirée sur les risques
- * associés au chargement,  à l'utilisation,  à la modification et/ou au
- * développement et à la reproduction du logiciel par l'utilisateur étant
- * donné sa spécificité de logiciel libre, qui peut le rendre complexe à
- * manipuler et qui le réserve donc à des développeurs et des professionnels
- * avertis possédant  des  connaissances  informatiques approfondies.  Les
- * utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
- * logiciel à leurs besoins dans des conditions permettant d'assurer la
- * sécurité de leurs systèmes et ou de leurs données et, plus généralement,
- * à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
  *
- * Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
- * pris connaissance de la licence CeCILL, et que vous en avez accepté les
- * termes.
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
  */
 
 package fr.zcraft.quartzlib.components.i18n.translators.gettext;
@@ -83,6 +85,20 @@ public class PluralForms {
         this.formsFunction = computeFormsFunction();
     }
 
+    /**
+     * For a given number, compute the plural index to use for the locale of this file.
+     *
+     * <p>Some plural scripts are very commons. For them, we hardcode native functions.
+     * We then do not depend on a JavaScript engine, and it's order of magnitude faster.
+     * If you can use them, it's always better.
+     *
+     * <p>This method can only work correctly with Plural-Forms listed at:
+     * http://www.gnu.org/software/gettext/manual/html_node/Plural-forms.html#Plural-forms,
+     * as well as POEdit-generated plural forms.
+     *
+     * @param count The count to compute plural for.
+     * @return The plural index.
+     */
     public int computePluralForm(long count) {
         final int index = this.formsFunction.apply(count);
 
@@ -93,6 +109,16 @@ public class PluralForms {
         }
     }
 
+    /**
+     * Computes the function to use to compute the plural form to use for a given count.
+     *
+     * <p>We first try to load a pure-Java implementation from a list of known plural scripts.
+     * If this fail, we try two JavaScript engines to compute the form. If this fail too, we
+     * fallback to English rules.
+     *
+     * @return a function that can be used to compute the plural index from a count.
+     * @see #computePluralForm(long) Method to use the computed function.
+     */
     private Function<Long, Integer> computeFormsFunction() {
         if (formsScript == null || formsScript.isEmpty()) {
             return formsFunctionFallback();
@@ -117,8 +143,25 @@ public class PluralForms {
         return function;
     }
 
+    /**
+     * Normalizes a forms script so we can compare them to a list of known scripts.
+     *
+     * <p>The normalized version is lowercase, without spaces, and without unnecessary
+     * surounding parenthesis.
+     *
+     * @param script The script.
+     * @return The normalized version.
+     */
     private String normalizeFormsScript(final String script) {
-        return script.toLowerCase(Locale.ROOT).replace(" ", "").trim();
+        final String normalized = script.toLowerCase(Locale.ROOT).replace(" ", "").trim();
+
+        // POEdit tends to add unnecessary parenthesis around the plural scripts. For them to correctly
+        // match our known scripts, we strip them.
+        if (normalized.startsWith("(") && normalized.endsWith(")")) {
+            return normalized.substring(1, normalized.length() - 1);
+        } else {
+            return normalized;
+        }
     }
 
     /**
@@ -126,7 +169,8 @@ public class PluralForms {
      * We then do not depend on a JavaScript engine, and it's order of magnitude faster.
      *
      * <p>This evaluate method can only work correctly with Plural-Forms listed at:
-     * http://www.gnu.org/software/gettext/manual/html_node/Plural-forms.html#Plural-forms
+     * http://www.gnu.org/software/gettext/manual/html_node/Plural-forms.html#Plural-forms as
+     * well as POEdit-generated plural forms.
      *
      * @return a Function to compute the plural index from the given count.
      */
@@ -141,26 +185,41 @@ public class PluralForms {
             // English, German, Dutch, Swedish, Danish, Norwegian, Faroese, Spanish, Portuguese, Italian,
             // Greek, Bulgarian, Finnish, Estonian, Hebrew, ahasa Indonesian, Esperanto, Hungarian, Turkish
             case "n!=1":
+            case "n<1||n>1": // Yup. POEdit can generate this.
                 return n -> n != 1 ? 1 : 0;
 
             case "n!=0":
                 return n -> n != 0 ? 1 : 0;
 
-            // Two forms, singular used for zero and one
-            // Brazilian Portuguese, French
+            case "n==0||n==1":
+                return n -> n == 0 || n == 1 ? 1 : 0;
+
             case "n>0":
                 return n -> n > 0 ? 1 : 0;
 
+            // Two forms, singular used for zero and one
+            // Brazilian Portuguese, French
             case "n>1":
                 return n -> n > 1 ? 1 : 0;
+
+            // Icelandic, Macedonian
+            case "n%10==1&&n%100!=11":
+                return n -> n % 10 == 1 && n % 100 != 11 ? 1 : 0;
+
+            case "n<=1||(n>=11&&n<=99)":
+                return n -> n <= 1 || (n >= 11 && n <= 99) ? 1 : 0;
 
             // Three forms, special case for zero
             // Latvian
             case "n%10==1&&n%100!=11?0:n!=0?1:2":
                 return n -> n % 10 == 1 && n % 100 != 11 ? 0 : (n != 0 ? 1 : 2);
 
+            // Filipino, Tagalog
+            case "n==1||n==2||n==3||(n%10!=4&&n%10!=6&&n%10!=9)":
+                return n -> n == 1 || n == 2 || n == 3 || (n % 10 != 4 && n % 10 != 6 && n % 10 != 9) ? 1 : 0;
+
             // Three forms, special cases for one and two
-            // Gaeilge (Irish)
+            // Gaeilge (Irish), Cornish, Nama, Northern Sami
             case "n==1?0:n==2?1:2":
                 return n -> n == 1 ? 0 : (n == 2 ? 1 : 2);
 
@@ -175,10 +234,16 @@ public class PluralForms {
                 return n -> n % 10 == 1 && n % 100 != 11 ? 0 : (n % 10 >= 2 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2);
 
             // Three forms, special cases for numbers ending in 1 and 2, 3, 4, except those ending in 1[1-4]
-            // Russian, Ukrainian, Belarusian, Serbian, Croatian
+            // Russian, Ukrainian, Serbian, Croatian, Belarusian, Bosnian
             case "n%10==1&&n%100!=11?0:n%10>=2&&n%10<=4&&(n%100<10||n%100>=20)?1:2":
                 return n -> n % 10 == 1 && n % 100 != 11 ? 0 :
                         (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2);
+
+            // Alternative for the above, sometime encountered
+            // Russian, Ukrainian, Serbian, Croatian, Belarusian, Bosnian
+            case "n%10==1&&n%100!=11?0:n%10>=2&&n%10<=4&&(n%100<12||n%100>14)?1:2":
+                return n -> n % 10 == 1 && n % 100 != 11 ? 0 :
+                        (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 12 || n % 100 >= 14) ? 1 : 2);
 
             // Three forms, special cases for 1 and 2, 3, 4
             // Czech, Slovak
@@ -192,10 +257,75 @@ public class PluralForms {
             case "n==1?0:n%10>=2&&n%10<=4&&(n%100<10||n%100>=20)?1:2":
                 return n -> n == 1 ? 0 : (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2);
 
+            // Polish (alternative)
+            case "n==1?0:n%10>=2&&n%10<=4&&(n%100<12||n%100>14)?1:2":
+                return n -> n == 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 12 || n % 100 > 14) ? 1 : 2;
+
+            // Colognian
+            case "n==0?0:n==1?1:2":
+                return n -> n == 0 ? 0 : n == 1 ? 1 : 2;
+
+            // Langi
+            case "n==0?0:(n==0||n==1)&&n!=0?1:2":
+                return n -> n == 0 ? 0 : (n == 0 || n == 1) && n != 0 ? 1 : 2;
+
+            // Lithuanian
+            case "n%10==1&&(n%100<11||n%100>19)?0:n%10>=2&&n%10<=9&&(n%100<11||n%100>19)?1:2":
+                return n -> n % 10 == 1 && (n % 100 < 11 || n % 100 > 19) ? 0 :
+                        n % 10 >= 2 && n % 10 <= 9 && (n % 100 < 11 || n % 100 > 19) ? 1 : 2;
+
+            // Latvian
+            case "n%10==0||(n%100>=11&&n%100<=19)?0:n%10==1&&n%100!=11?1:2":
+                return n -> n % 10 == 0 || (n % 100 >= 11 && n % 100 <= 19) ? 0 : n % 10 == 1 && n % 100 != 11 ? 1 : 2;
+
+            // Romanian, Moldavian
+            case "n==1?0:n==0||(n!=1&&n%100>=1&&n%100<=19)?1:2":
+                return n -> n == 1 ? 0 : n == 0 || (n != 1 && n % 100 >= 1 && n % 100 <= 19) ? 1 : 2;
+
+            case "n==0||n==1?0:n>=2&&n<=10?1:2":
+                return n -> n == 0 || n == 1 ? 0 : n >= 2 && n <= 10 ? 1 : 2;
+
             // Four forms, special case for one and all numbers ending in 02, 03, or 04
             // Slovenian
             case "n%100==1?0:n%100==2?1:n%100==3||n%100==4?2:3":
                 return n -> n % 100 == 1 ? 0 : (n % 100 == 2 ? 1 : (n % 100 == 3 || n % 100 == 4 ? 2 : 3));
+
+            // Slovenian (alternative), Upper Sorbian
+            case "n%100==1?0:n%100==2?1:n%100>=3&&n%100<=4?2:3":
+                return n -> n % 100 == 1 ? 0 : n % 100 == 2 ? 1 : n % 100 >= 3 && n % 100 <= 4 ? 2 : 3;
+
+            // Manx
+            case "n%10==1?0:n%10==2?1:n%100==0||n%100==20||n%100==40||n%100==60||n%100==80?2:3":
+                return n -> n % 10 == 1 ? 0 : n % 10 == 2 ? 1 :
+                        n % 100 == 0 || n % 100 == 20 || n % 100 == 40 || n % 100 == 60 || n % 100 == 80 ? 2 : 3;
+
+            // Hebrew
+            case "n==1?0:n==2?1:n>10&&n%10==0?2:3":
+                return n -> n == 1 ? 0 : n == 2 ? 1 : n > 10 && n % 10 == 0 ? 2 : 3;
+
+            // Scottish Gaelic
+            case "n==1||n==11?0:n==2||n==12?1:(n>=3&&n<=10)||(n>=13&&n<=19)?2:3":
+                return n -> n == 1 || n == 11 ? 0 :
+                        n == 2 || n == 12 ? 1 : (n >= 3 && n <= 10) || (n >= 13 && n <= 19) ? 2 : 3;
+
+            // Maltese
+            case "n==1?0:n==0||(n%100>=2&&n%100<=10)?1:n%100>=11&&n%100<=19?2:3":
+                return n -> n == 1 ? 0 :
+                        n == 0 || (n % 100 >= 2 && n % 100 <= 10) ? 1 : n % 100 >= 11 && n % 100 <= 19 ? 2 : 3;
+
+            // Breton (are you serious??)
+            case "n%10==1&&n%100!=11&&n%100!=71&&n%100!=91?0:n%10==2&&n%100!=12&&n%100!=72&&n%100!=92?1:((n%10>=3&&n%10"
+                    + "<=4)||n%10==9)&&(n%100<10||n%100>19)&&(n%100<70||n%100>79)&&(n%100<90||n%100>99)?2:n!=0&&n%10000"
+                    + "00==0?3:4":
+                return n -> n % 10 == 1 && n % 100 != 11 && n % 100 != 71 && n % 100 != 91 ? 0 :
+                        n % 10 == 2 && n % 100 != 12 && n % 100 != 72 && n % 100 != 92 ? 1 :
+                                ((n % 10 >= 3 && n % 10 <= 4) || n % 10 == 9) && (n % 100 < 10 || n % 100 > 19)
+                                        && (n % 100 < 70 || n % 100 > 79) && (n % 100 < 90 || n % 100 > 99) ? 2 :
+                                        n != 0 && n % 1000000 == 0 ? 3 : 4;
+
+            // Irish
+            case "n==1?0:n==2?1:n>=3&&n<=6?2:n>=7&&n<=10?3:4":
+                return n -> n == 1 ? 0 : n == 2 ? 1 : n >= 3 && n <= 6 ? 2 : n >= 7 && n <= 10 ? 3 : 4;
 
             // Six forms, special cases for one, two, all numbers ending in 02, 03, … 10, all numbers ending in 11 … 99,
             // and others
@@ -203,6 +333,14 @@ public class PluralForms {
             case "n==0?0:n==1?1:n==2?2:n%100>=3&&n%100<=10?3:n%100>=11?4:5":
                 return n -> n == 0 ? 0 :
                         (n == 1 ? 1 : (n == 2 ? 2 : (n % 100 >= 3 && n % 100 <= 10 ? 3 : (n % 100 >= 11 ? 4 : 5))));
+
+            case "n==0?0:n==1?1:n==2?2:n%100>=3&&n%100<=10?3:n%100>=11&&n%100<=99?4:5":
+                return n -> n == 0 ? 0 :
+                        (n == 1 ? 1 : (n == 2 ? 2 :
+                                (n % 100 >= 3 && n % 100 <= 10 ? 3 : (n % 100 >= 11 && n % 100 <= 99 ? 4 : 5))));
+
+            case "n==0?0:n==1?1:n==2?2:n==3?3:n==6?4:5":
+                return n -> n == 0 ? 0 : n == 1 ? 1 : n == 2 ? 2 : n == 3 ? 3 : n == 6 ? 4 : 5;
 
             default:
                 return null;
@@ -267,10 +405,10 @@ public class PluralForms {
      */
     private Function<Long, Integer> formsFunctionFallback() {
         PluginLogger.warning(
-                  "Unknown plural rule “{0}”; without JavaScript engine available, we'll fallback to English "
-                + "pluralization rules. If you want your language's plural rules supported without JavaScript "
-                + "engine, please open an issue with your language and its plural rules at "
-                + "https://github.com/zDevelopers/QuartzLib/issues.",
+                "Unknown plural rule “{0}”; without JavaScript engine available, we'll fallback to English "
+                        + "pluralization rules. If you want your language's plural rules supported without JavaScript "
+                        + "engine, please open an issue with your language and its plural rules at "
+                        + "https://github.com/zDevelopers/QuartzLib/issues.",
                 formsScript
         );
 
